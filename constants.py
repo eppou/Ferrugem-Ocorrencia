015be74ac19da480a1387de1b0501e6f0d612ba4
Cristiano Kemmer <criskemmer@gmail.com>
2023-06-27 23:15:01 -0300

    importação dos shapefiles das cidades do PR para o PostgreSQL

---
+++ constants.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+INPUT_PATH = Path(__file__).parent / "input/"
+DB_STRING = "postgresql://crke:123456@localhost:5432/mestrado_pesquisa"
---
+++ procedures/import_city_shapefiles.py
@@ -0,0 +1,28 @@
+from pprint import pprint
+
+import geopandas as gpd
+import seaborn as sns
+from sqlalchemy import create_engine
+
+from constants import INPUT_PATH, DB_STRING
+
+
+def import_postgres():
+    sns.set(style="whitegrid", palette="pastel", color_codes=True)
+    sns.mpl.rc("figure", figsize=(10, 6))
+
+    path = INPUT_PATH / "shapefiles_LimitesMunicipaisPR/LimitesMunicipaisPR.shp"
+    # sf = shp.Reader(path)
+
+    geodf = gpd.read_file(path, crs="epsg:4674")
+
+    geodf = geodf[["nome", "geometry"]]
+    geodf.columns = ["city_name", "geometry"]
+    geodf.set_crs("epsg:4674", inplace=True, allow_override=True)
+    pprint(geodf.columns)
+    pprint(geodf.crs)
+
+    db_con_engine = create_engine(DB_STRING)
+    geodf.to_postgis("city_shapefiles", db_con_engine, if_exists="append")
+
+    pprint(geodf.sample(5))
