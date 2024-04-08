72cfb9ab8e356170b160b1b856d1df6ff2386670
Cristiano Kemmer <criskemmer@gmail.com>
2024-04-08 01:19:08 -0300

    Filtro de safras

--- main.py
+++ main.py
@@ -16,38 +16,68 @@ def match_and_run():
     if len(sys.argv) < 2:
         return
 
+    safras = [
+        "2021/2022",
+        "2020/2021",
+        "2019/2020",
+        "2018/2019",
+        "2017/2018",
+        "2016/2017",
+        "2015/2016",
+        "2014/2015",
+        "2013/2014",
+        "2012/2013",
+        "2011/2012",
+        "2010/2011",
+    ]
+    safras_param = [safras]
+
     command = sys.argv[1]
     match command:
         case "train_test_model":
             run(ttm.run)
+
         case "test_baseline_model":
             run(baseline.run)
+
         case "prepare_severity_per_occurrence":
             run(spo.run)
+
         case "prepare_precipitation":
             run(pp.run)
+
         case "download_precipitation":
             run(dpp.run)
+
         case "prepare_occurrence_instances":
             run(poi.run)
+
         case "prepare_occurrence_features":
             run(pof.run)
+
         case "pipeline":
             run(poi.run)
             run(pof.run)
             run(baseline.run)
             run(ttm.run)
 
+        case "pipeline_results":
+            run(baseline.run, safras_param)
+            run(ttm.run, safras_param)
+
         case _:
             print(f"Unknown command: {command}")
 
 
-def run(object_to_run: Callable):
+def run(object_to_run: Callable, args: list = None):
     print(datetime.now().strftime("Execution started at %Y-%m-%d %I:%M:%S %p"))
     print()
     start = time.time()
 
-    object_to_run()
+    if args is None:
+        object_to_run()
+    else:
+        object_to_run(*args)
 
     end = time.time()
 
